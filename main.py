a840a1ec59db224fa451b24d001a4364d342463f
yejin song <songyeajin87@naver.com>
2025-07-15 10:20:26 +0900

    fast api development preparation

---
+++ main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = [
+    "http://localhost:5173",  # 프론트엔드 개발 서버
+    "http://127.0.0.1:5173",  # 프론트엔드 개발 서버 (IP 주소)
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/hello")
+def hello():
+    return {"message":"안녕하세요 파이보"}
