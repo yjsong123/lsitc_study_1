0bfb6f9f326e9aa53f67798458441b0fc9b76908
yejin song <songyeajin87@naver.com>
2025-07-16 09:48:10 +0900

    commit

---
+++ domain/question/question_schema.py
@@ -0,0 +1,11 @@
+import datetime
+from pydantic import BaseModel
+
+class Question(BaseModel):
+    id: int
+    subject: str | None = None
+    content: str
+    create_date: datetime.datetime
+
+    class Config:
+        orm_mode = True
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 
+from domain.question import question_router
+
 app = FastAPI()
 
 origins = [
@@ -16,6 +18,9 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@app.get("/hello")
-def hello():
-    return {"message":"안녕하세요 파이보"}
+# HELLO API
+# @app.get("/hello")
+# def hello():
+#     return {"message":"안녕하세요 파이보"}
+
+app.include_router(question_router.router)
